62d4d9611cd6538a52e4813e722d005468d335e9
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-02-08 01:13:04 +0900

    mapSearch

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import axios from 'axios'
 
-export default function App() {
+const App = () => {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.35948,
+    longitude: 127.37895,
+    latitudeDelta: 0.003,
+    longitudeDelta: 0.003,
+  })
+
+  useEffect(()=>{
+    searchTest()
+  },[])
+
+  const searchTest = async () => {
+    const key = "0d354750cc5df9c00497abcd507c89d5"
+    const searching = "쿠오리노"
+    const coord = await axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${searching}`, {
+      headers: { Authorization: `KakaoAK ${key}` }
+    })
+    const location = coord.data.documents[0]
+    const xCoord = location.x
+    const yCoord = location.y
+
+    setMapRegion({
+      ...mapRegion,
+      latitude: Number(yCoord),
+      longitude: Number(xCoord),
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='사무실' />
+      </MapView>
     </View>
-  );
+  )
 }
-
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+})
