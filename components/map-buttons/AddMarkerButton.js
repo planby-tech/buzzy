6788ce623d6b451905999914706f4f3c6b42e23e
two-vs-han <daehan1028@naver.com>
2022-02-23 20:27:29 +0900

    style: Fix the shadow a little bit

--- components/map-buttons/AddMarkerButton.js
+++ components/map-buttons/AddMarkerButton.js
@@ -63,8 +63,19 @@ const styles = StyleSheet.create({
     left: wp(4.3),
     bottom: hp(4),
     borderRadius: 8,
-    shadowColor: "#333",
-    shadowOpacity: 0.5,
-    elevation: 5,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0,
+        },
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
   },
 });
