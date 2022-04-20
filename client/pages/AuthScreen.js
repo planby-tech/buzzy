3f8f84b67ef5c04f97a0203a81d20e506eb0886f
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-04-20 23:11:37 +0900

    add group model, controller

--- client/pages/AuthScreen.js
+++ client/pages/AuthScreen.js
@@ -10,7 +10,9 @@ import {
 } from "react-native";
 
 const API_URL =
-  Platform.OS === "ios" ? "http://192.0.0.1:3001" : "http://192.0.0.1:3001";
+  Platform.OS === "ios"
+    ? "http://192.168.219.101:3001"
+    : "http://192.168.219.103:3001";
 
 const AuthScreen = () => {
   const [email, setEmail] = useState("");
