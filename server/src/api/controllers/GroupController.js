25fa8129d6717472d9bb85e7751ec9005242ea85
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-04-25 02:08:22 +0900

    add: group controller

--- server/src/api/controllers/GroupController.js
+++ server/src/api/controllers/GroupController.js
@@ -1,1 +1,22 @@
 import Group from "../../models/Group.js";
+import crypto from "crypto";
+
+const createGroup = (req, res, next) => {
+  if (!groupValidCheck(req)) {
+    return groupValidMsq(req, res);
+  } else {
+    return Group.create({
+      name: req.body.name,
+      description: req.body.description,
+      userNumber: 1,
+      groupCode: crypto.randomUUID(),
+    })
+      .then(() => {
+        res.status(200).json({ message: "group created" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(502).json({ message: "error while creating the croup" });
+      });
+  }
+};
