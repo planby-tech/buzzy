23eb3edc4df4c839c74b2f81f6e42ce5e53d9e5f
daehanLee <daehan1028@gmail.com>
2022-05-16 16:29:56 +0900

    feat: Add AddGroupScreen

--- client/screens/garden/AddGroupScreen.js
+++ client/screens/garden/AddGroupScreen.js
@@ -1,11 +1,39 @@
 import { Formik } from "formik";
-import { View } from "react-native";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import CreateGroupScreen from "./CreateGroupScreen";
+import JoinGroupScreen from "./JoinGroupScreen";
 
 const AddGroupScreen = ({ navigation }) => {
+  const [type, setType] = useState("create");
   return (
     <View>
-      <CreateGroupScreen navigation={navigation} />
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <TouchableOpacity
+          style={{
+            width: 150,
+            height: 50,
+            borderColor: "black",
+            borderWidth: 2,
+          }}
+          onPress={() => setType("create")}
+        >
+          <Text>그룹 생성하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: 150,
+            height: 50,
+            borderColor: "black",
+            borderWidth: 2,
+          }}
+          onPress={() => setType("join")}
+        >
+          <Text>그룹 참여하기</Text>
+        </TouchableOpacity>
+      </View>
+      {type === "create" && <CreateGroupScreen navigation={navigation} />}
+      {type === "join" && <JoinGroupScreen navigation={navigation} />}
     </View>
   );
 };
